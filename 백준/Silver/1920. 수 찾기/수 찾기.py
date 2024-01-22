28c9f5ab0e87e0bff4fc181cf5888dadc112922e
하승진 <110087099+j2an777@users.noreply.github.com>
2024-01-22 14:58:40 +0900

    [Silver IV] Title: 수 찾기, Time: 408 ms, Memory: 47016 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,25 +1,29 @@
-a = int(input())
-b = list(map(int, input().split()))
-c = int(input())
-d = list(map(int, input().split()))
-b.sort()
-
-def findtarget(i) :
-    start = 0
-    end = a - 1
+def binary_search(array, target, start, end) :
     
     while start <= end :
-        mid = (start + end) // 2  #이분탐색 위한 중앙인덱스
+        mid = (start + end) // 2
         
-        if b[mid] == i :
+        if array[mid] == target :
             return True
-        elif i < b[mid] :
-            end = mid - 1
-        else :
+        
+        elif array[mid] < target :
             start = mid + 1
+            
+        else :
+            end = mid - 1
+            
+    return False
 
-for i in range(len(d)) :
-    if (findtarget(d[i])) :
+n = int(input())
+nBox = list(map(int, input().split()))
+    
+m = int(input())
+mBox = list(map(int, input().split()))
+
+nBox.sort()
+
+for num in mBox :
+    if binary_search(nBox, num, 0, n-1) :
         print(1)
     else :
         print(0)
