deb4ef8742ae09e04d27fb155b37c09dc1c13ce5
하승진 <110087099+j2an777@users.noreply.github.com>
2024-02-04 22:43:47 +0900

    [Silver I] Title: 오르막 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,14 +1,11 @@
 n = int(input())
-dp = [[0 for _ in range(10)] for _ in range(n)]
+dp = [1] * 10
+res = 0
 mod = 10007
 
-for i in range(10) :
-    dp[0][i] = 1
-
-for i in range(1, n) :
-    for j in range(10) :
-        for k in range(0, j+1) :
-            dp[i][j] += dp[i-1][k]
-            
-res = sum(dp[-1]) % mod
+for i in range(n-1) :
+    for j in range(1, 10) :
+        dp[j] += dp[j-1]
+        
+res = sum(dp) % mod
 print(res)
