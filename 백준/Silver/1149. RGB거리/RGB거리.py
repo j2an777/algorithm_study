d9954038ee32f2408f05f99012873446fc7704e9
하승진 <110087099+j2an777@users.noreply.github.com>
2024-02-05 21:51:03 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -4,9 +4,9 @@ dp = []
 for i in range(n) :
     dp.append(list(map(int, input().split())))
     
-for i in range(1, n) :
-    dp[i][0] += min(dp[i-1][1], dp[i-1][2])
-    dp[i][1] += min(dp[i-1][0], dp[i-1][2])
-    dp[i][2] += min(dp[i-1][0], dp[i-1][1])
+for i in range(0, n-1) :
+    dp[i+1][0] += min(dp[i][1], dp[i][2])
+    dp[i+1][1] += min(dp[i][0], dp[i][2])
+    dp[i+1][2] += min(dp[i][0], dp[i][1])
     
 print(min(dp[n-1]))
